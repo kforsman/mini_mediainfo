f608902d7f51a51c3b8900bf943d8ddaf3a5c31e
Karl Forsman <kforsman@wurl.com>
2013-09-19 01:03:19 -0700

    some class methods for getting info about mediainfo

--- lib/mini_mediainfo.rb
+++ lib/mini_mediainfo.rb
@@ -1,5 +1,21 @@
 require "mini_mediainfo/version"
+require 'open3'
 
 module MiniMediainfo
-  # Your code goes here...
+
+  def self.platform_supported?
+    /(darwin|linux|unix)/ =~ RUBY_PLATFORM
+  end
+
+  def self.mediainfo_version
+    cmd = "mediainfo --version"
+    out = Open3.popen3(cmd) { |stdin, stdout, stderr| stdout.read }
+    out.gsub("\n","")
+  end
+
+  def self.mediainfo_binary
+    cmd = "which mediainfo"
+    Open3.popen3(cmd) { |stdin, stdout, stderr| stdout.read }
+  end
+
 end
